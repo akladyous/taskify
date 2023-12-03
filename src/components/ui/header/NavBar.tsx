4ece52524d6a0112a37c79cdd00fd8e229fe4b03
Boula Akladyous <akladyous@gmail.com>
2023-12-03 12:38:41 -0500

    Untracked files on taskify

--- src/components/ui/header/NavBar.tsx
+++ src/components/ui/header/NavBar.tsx
@@ -42,7 +42,8 @@ export default function NavBar() {
             {navItems.map((item, i) => (
               <li
                 key={i}
-                className='block md:p-4 hover:md:border-l-0 hover:md:border-b-2 | py-1 my-1 pl-1 hover:border-l-4 hover:border-gray-300'>
+                className='block md:p-4 hover:md:border-l-0 hover:md:border-b-2 | py-1 my-1 pl-1 hover:border-l-4 hover:border-gray-300 items-cern
+                '>
                 <Link href={item.href} className={styles['nav-item']}>
                   {item.name}
                 </Link>
