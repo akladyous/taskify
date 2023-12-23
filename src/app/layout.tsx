ad1c57347a5110f3d1ef237897d2b68ee4b788cd
Boula Akladyous <akladyous@gmail.com>
2023-12-23 09:18:51 -0500

    Untracked files on taskify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,28 @@
 import { ClerkProvider } from '@clerk/nextjs';
-import { Inter } from 'next/font/google';
+import { Inter, Roboto, Roboto_Mono } from 'next/font/google';
 import '@/app/globals.css';
 import type { Metadata } from 'next';
 import { ThemeProvider } from '@/components/theme/ThemeProvider';
 import NavBar from '@/components/ui/header/NavBar';
 
 const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+  subsets: ['latin'],
+});
 
-export const metadata: Metadata = {
-  title: 'Taskify Application',
-  description: '',
-};
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  variable: '--font-roboto-mono',
+  display: 'swap',
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.className}  ${roboto_mono.variable}`}>
         <ClerkProvider
           appearance={{ elements: { footer: 'hidden', footerAction: { display: 'none' } } }}>
           <ThemeProvider attribute='class' defaultTheme='light'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
     <div className='h-[2048px] border m-2'>
-      <p className='' style={{}}>
+      <p className='roboto' style={{}}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod at eius eum, facere ipsa qui
         praesentium ab vero deleniti delectus eveniet doloribus minus ex pariatur reiciendis quos
         officia optio aliquid.
