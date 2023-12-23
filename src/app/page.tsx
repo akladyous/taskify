92b15e037061bfaf7f3c4474d467e041689fbba8
Boula Akladyous <akladyous@gmail.com>
2023-12-23 11:07:25 -0500

    Untracked files on taskify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className='h-[2048px] border m-2'>
       <p className='item'>sit ipsum exercitationem?</p>
       <p className='item'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <p className='item' style={{}}>
+      <p className='item'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod at eius eum, facere ipsa qui
         praesentium ab vero deleniti delectus eveniet doloribus minus ex pariatur reiciendis quos
         officia optio aliquid.
