e6b0be74cc40c2451f8a11c53928e8df143b2811
Boula Akladyous <akladyous@gmail.com>
2023-12-23 09:22:48 -0500

    Untracked files on taskify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,24 +5,28 @@ import type { Metadata } from 'next';
 import { ThemeProvider } from '@/components/theme/ThemeProvider';
 import NavBar from '@/components/ui/header/NavBar';
 
-const inter = Inter({ subsets: ['latin'] });
-const roboto = Roboto({
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  display: 'swap',
+const inter = Inter({
   subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
 });
-
 const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
   variable: '--font-roboto-mono',
   display: 'swap',
 });
 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={`${inter.className}  ${roboto_mono.variable}`}>
+      <body className={`${inter.className}  ${roboto_mono.variable} ${roboto.variable}`}>
         <ClerkProvider
           appearance={{ elements: { footer: 'hidden', footerAction: { display: 'none' } } }}>
           <ThemeProvider attribute='class' defaultTheme='light'>
