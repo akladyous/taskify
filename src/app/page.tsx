3e38f95fd6c4748deaa3fa9ed3986e435103e24a
Boula Akladyous <akladyous@gmail.com>
2023-12-27 12:04:18 -0500

    Untracked files on taskify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <main className='h-[2048px] border m-2'>
-      <p className='item'>
+    <main className=' border m-2'>
+      <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod at eius eum, facere ipsa qui
         praesentium ab vero deleniti delectus eveniet doloribus minus ex pariatur reiciendis quos
         officia optio aliquid.
