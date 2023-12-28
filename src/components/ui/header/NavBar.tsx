0ab98dc334edb6ef5e19bcab8460b390cb905929
Boula Akladyous <akladyous@gmail.com>
2023-12-28 13:32:31 -0500

    Untracked files on taskify

--- src/components/ui/header/NavBar.tsx
+++ src/components/ui/header/NavBar.tsx
@@ -7,7 +7,6 @@ import { ThemeToggler } from '@/components/theme/ThemeToggler';
 import { UserButton } from '@clerk/nextjs';
 import { navItems } from '@/lib/appConfig';
 import CurrentUser from './CurrentUser';
-import styles from './navbar.module.css';
 
 export default function NavBar() {
   const hamburgerRef = useRef<any>();
@@ -43,9 +42,7 @@ export default function NavBar() {
               <li
                 key={i}
                 className='block md:p-4 hover:md:border-l-0 hover:md:border-b-2 | py-1 my-1 pl-1 hover:border-l-4 hover:border-gray-300'>
-                <Link href={item.href} className={styles['nav-item']}>
-                  {item.name}
-                </Link>
+                <Link href={item.href}>{item.name}</Link>
               </li>
             ))}
           </ul>
