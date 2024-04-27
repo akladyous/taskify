59d8ca7ddbcfbfb2427a98b947d27f8ce2565a26
Boula Akladyous <akladyous@gmail.com>
2024-04-27 10:36:45 -0400

    Untracked files on taskify

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -7,3 +7,7 @@ import { type ThemeProviderProps } from 'next-themes/dist/types';
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+1;
+2;
+3;
+4;
