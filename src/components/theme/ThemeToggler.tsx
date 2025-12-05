00b2ccf2bbc9e03c0ef45772ada0fb39317b977a
Boula Akladyous <akladyous@gmail.com>
2025-12-05 09:17:14 -0500

    Untracked files on taskify

--- src/components/theme/ThemeToggler.tsx
+++ src/components/theme/ThemeToggler.tsx
@@ -7,20 +7,20 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
 export function ThemeToggler() {
-  const { theme, setTheme } = useTheme();
+    const { theme, setTheme } = useTheme();
 
-  const toggleTheme = useCallback(() => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  }, [theme]);
+    const toggleTheme = useCallback(() => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    }, [theme]);
 
-  return (
-    <Button className="rounded-full" variant="outline" size="icon" onClick={toggleTheme}>
-      {/*
+    return (
+        <Button className="rounded-full" variant="outline" size="icon" onClick={toggleTheme}>
+            {/*
         <SunIcon className=' scale-100 transition-all  dark:scale-0' size={25} />
         <MoonIcon className='scale-0   transition-all  dark:scale-100 absolute' size={25} />
         <span className='sr-only'>Toggle theme</span>
       */}
-      {theme === "dark" ? <SunIcon size={25} /> : <MoonIcon size={25} />}
-    </Button>
-  );
+            {theme === "dark" ? <SunIcon size={25} /> : <MoonIcon size={25} />}
+        </Button>
+    );
 }
