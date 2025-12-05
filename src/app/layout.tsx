470316ebe45b0a02de59d572a8ec34891aa5cee0
Boula Akladyous <akladyous@gmail.com>
2025-12-05 09:15:52 -0500

    Untracked files on taskify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,29 +6,33 @@ import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import NavBar from "@/components/ui/header/NavBar";
 
 const inter = Inter({
-    subsets: ["latin"],
-    variable: "--font-inter",
-    display: "swap",
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
 });
 const roboto = Roboto({
-    weight: "400",
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-roboto",
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={`${inter.className}  ${inter.variable} ${roboto.variable}`}>
-                <ClerkProvider appearance={{ elements: { footer: "hidden", footerAction: { display: "none" } } }}>
-                    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-                        <NavBar />
-                        {children}
-                    </ThemeProvider>
-                </ClerkProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}  ${inter.variable} ${roboto.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: { footer: "hidden", footerAction: { display: "none" } },
+          }}
+        >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <NavBar />
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
 //
--- src/components/theme/ThemeToggler.tsx
+++ src/components/theme/ThemeToggler.tsx
@@ -1,26 +1,26 @@
-'use client';
+"use client";
 
-import { useCallback } from 'react';
+import { useCallback } from "react";
 
-import { MoonIcon, SunIcon } from 'lucide-react';
-import { useTheme } from 'next-themes';
-import { Button } from '@/components/ui/button';
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 
 export function ThemeToggler() {
   const { theme, setTheme } = useTheme();
 
   const toggleTheme = useCallback(() => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    setTheme(theme === "dark" ? "light" : "dark");
   }, [theme]);
 
   return (
-    <Button className='rounded-full' variant='outline' size='icon' onClick={toggleTheme}>
+    <Button className="rounded-full" variant="outline" size="icon" onClick={toggleTheme}>
       {/*
         <SunIcon className=' scale-100 transition-all  dark:scale-0' size={25} />
         <MoonIcon className='scale-0   transition-all  dark:scale-100 absolute' size={25} />
         <span className='sr-only'>Toggle theme</span>
       */}
-      {theme === 'dark' ? <SunIcon size={25} /> : <MoonIcon size={25} />}
+      {theme === "dark" ? <SunIcon size={25} /> : <MoonIcon size={25} />}
     </Button>
   );
 }
